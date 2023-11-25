32932768ce006ae815c391d2b56152cff8c9922c
Nam junseong <danielnam0801@gmail.com>
2023-11-26 00:06:33 +0900

    ADD_Stage-Thron 2

---
+++ Assets/01.Scripts/Utils/StageStructureConvertSystem/ObstacleObjectUnit.cs
@@ -0,0 +1,12 @@
+using StageStructureConvertSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleObjectUnit : StructureObjectUnitBase
+{
+    public override void TransformSynchronization(EAxisType axisType)
+    {
+        base.TransformSynchronization(axisType);
+    }
+}
