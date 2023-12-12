fe0756d38dbe741463afcc07cc46ab41aac461b8
Nam junseong <danielnam0801@gmail.com>
2023-12-12 09:54:18 +0900

    ADD_StageManager

---
+++ Assets/01.Scripts/Managers/StageManager.cs
@@ -0,0 +1,17 @@
+using ManagingSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : BaseManager<CameraManager>
+{
+    public override void StartManager()
+    {
+        
+    }
+
+    public override void UpdateManager()
+    {
+            
+    }
+}
