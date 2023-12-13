5f57e21cd126e9f529ec7467bdce857fb34e2b6c
ehdbs28 <doyun5373@naver.com>
2023-12-13 18:46:43 +0900

    FEAT_그림 오브젝트 기믹 개발 완료
    - 일단 간단하게 작성해놔서 나중에 로직은 바꿀수도 있을 것 같음

---
+++ Assets/01.Scripts/StageObjects/PictureObject/PictureObject.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using StageStructureConvertSystem;
+
+public class PictureObject : StructureObjectUnitBase
+{
+    private List<PictureUnit> _units;
+
+    public override void Init(StructureConverter converter)
+    {
+        _units = new List<PictureUnit>();
+        transform.GetComponentsInChildren(_units);
+        foreach (var unit in _units)
+        {
+            unit.Init();
+        }
+        base.Init(converter);
+    }
+
+    public override void ObjectSetting()
+    {
+        UnitSetting(_objectInfo.axis);
+        base.ObjectSetting();
+    }
+
+    private void UnitSetting(EAxisType axis)
+    {
+        foreach (var unit in _units)
+        {
+            unit.ChangeAxis(axis);
+        }
+    }
+
+    public override void ColliderSetting()
+    {
+        base.ColliderSetting();
+
+        if (_objectInfo.axis != EAxisType.Y && _objectInfo.axis != EAxisType.NONE)
+        {
+            _collider.isTrigger = true;
+        }
+    }
+}
