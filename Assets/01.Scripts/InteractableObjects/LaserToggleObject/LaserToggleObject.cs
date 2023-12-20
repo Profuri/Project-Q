9072284b7fa20ee128bc453d3fbaf1c787d251a1
ehdbs28 <doyun5373@naver.com>
2023-12-20 19:41:08 +0900

    CHORE_레이저 토글 오브젝트 디테일 챙기기

--- Assets/01.Scripts/InteractableObjects/LaserToggleObject/LaserToggleObject.cs
+++ Assets/01.Scripts/InteractableObjects/LaserToggleObject/LaserToggleObject.cs
@@ -5,9 +5,47 @@ using UnityEngine;
 public class LaserToggleObject : InteractableObject
 {
     [SerializeField] private InteractableObject _affectedObject;
-    
+
+    private Light _pointLight;
+
+    private const float ToggleCancelDelay = 0.1f;
+    private bool _isToggle;
+    private float _lastToggleTime;
+
+    public void Awake()
+    {
+        _pointLight = transform.Find("Point Light").GetComponent<Light>();
+        Toggled(false);
+    }
+
+    private void Update()
+    {
+        if (!_isToggle)
+        {
+            return;
+        }
+
+        if (_lastToggleTime + ToggleCancelDelay <= Time.time)
+        {
+            Toggled(false);
+        }
+    }
+
+    private void Toggled(bool value)
+    {
+        _isToggle = value;
+        _pointLight.enabled = value;
+
+        if (value)
+        {
+            _lastToggleTime = Time.time;
+        }
+    }
+
     public override void OnInteraction(StructureObjectUnitBase communicator, bool interactValue, params object[] param)
     {
+        Toggled(true);
+        
         if (_affectedObject is not null)
         {
             _affectedObject.OnInteraction(communicator, interactValue);
