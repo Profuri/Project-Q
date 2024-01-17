a2452261559e7c806c0d4f606e178e1b0a49ec9b
ehdbs28 <doyun5373@naver.com>
2024-01-17 21:05:37 +0900

    FEAT_포탈 오브젝트 벽에 붙는 식으로 수정

--- Assets/01.Scripts/InteractableObjects/PortalObject/PortalObject.cs
+++ Assets/01.Scripts/InteractableObjects/PortalObject/PortalObject.cs
@@ -8,30 +8,61 @@ public class PortalObject : InteractableObject
     [SerializeField] private PortalObject _linkedPortal;
     [SerializeField] private EAxisType _portalAxis;
 
+    private StructureObjectUnitBase _parentUnit;
+
+    private void Awake()
+    {
+        _parentUnit = transform.parent.GetComponent<StructureObjectUnitBase>();
+    }
+
     public override void OnInteraction(StructureObjectUnitBase communicator, bool interactValue, params object[] param)
     {
         if (communicator is PlayableObjectUnit playerUnit)
         {
-            var destPos = _linkedPortal.transform.localPosition;
-
+            var destPos = _linkedPortal._parentUnit.transform.localPosition;
+            var dir = Vector3.zero;
+            
             switch (_linkedPortal._portalAxis)
             {
                 case EAxisType.X:
-                    destPos += Vector3.left;
+                    dir = Vector3.right;
                     break;
                 case EAxisType.Y:
-                    destPos += Vector3.up;
+                    dir += Vector3.up;
                     break;
                 case EAxisType.Z:
-                    destPos += Vector3.forward;
+                    dir += Vector3.back;
                     break;
             }
             
             playerUnit.SetObjectInfo(
-                destPos,
+                destPos + dir,
                 playerUnit.transform.rotation,
                 playerUnit.transform.localScale
             );
         }
     }
+    
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        switch (_portalAxis)
+        {
+            case EAxisType.X:
+                transform.localScale = new Vector3(0, 1, 1);
+                transform.localPosition = new Vector3(0.51f, 0f, 0f);
+                break;
+            case EAxisType.Y:
+                transform.localScale = new Vector3(1, 0, 1);
+                transform.localPosition = new Vector3(0f, 0.51f, 0f);
+                break;
+            case EAxisType.Z:
+                transform.localScale = new Vector3(1, 1, 0);
+                transform.localPosition = new Vector3(0f, 0f, 0.51f);
+                break;
+        }
+    }
+
+#endif
 }
