c3a8e0b3630b41f2056b7ea1c5a6156a31458bf0
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2024-02-14 19:23:13 +0900

    ADD_InputField

--- Assets/Fabgrid/Scripts/Editor/Panels/Fields/PanelField.cs
+++ Assets/Fabgrid/Scripts/Editor/Panels/Fields/PanelField.cs
@@ -14,6 +14,8 @@ public abstract class PanelField
 
     protected PanelField(VisualElement root,VisualTreeAsset field, FieldInfo info)
     {
+        //추상 클래스 생성자가 실행이 되는지 궁금함
+        Debug.Log("CreatePanelField");
         _root = root;
         
         //루트에 자식으로 넣는데 기존에 있는 자식이 사라지고 있는 것 같음
@@ -21,6 +23,7 @@ public abstract class PanelField
         _root?.Add(container);
 
         _fieldRoot = container;
+        
         _label = container.Q<Label>("Label");
         if (_label == null)
         {
