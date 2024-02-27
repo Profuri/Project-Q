718962a230a6decd41ea96b18538eac8546bbcbf
ehdbs28 <doyun5373@naver.com>
2024-02-27 21:22:04 +0900

    FEAT_챕터 심볼 오브젝트 돌아가기

--- Assets/01.Scripts/Chapter/Chapter.cs
+++ Assets/01.Scripts/Chapter/Chapter.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 public class Chapter : InteractableObject
 {
     [SerializeField] private ChapterData _data;
-    
+    [SerializeField] private float _symbolRotateSpeed;
+
+    private Transform _symbolTrm;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _symbolTrm = transform.Find("Symbol");
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (_symbolTrm is not null)
+        {
+            _symbolTrm.Rotate(0, _symbolRotateSpeed, 0, Space.World);
+        }
+    }
+
     public override void OnInteraction(ObjectUnit communicator, bool interactValue, params object[] param)
     {
         SceneControlManager.Instance.LoadScene(SceneType.Stage, () =>
