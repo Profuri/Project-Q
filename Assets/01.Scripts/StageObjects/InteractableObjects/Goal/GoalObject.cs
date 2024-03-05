70ab702560c8c0e03fdd6bc0978b2ab209f53d38
ehdbs28 <doyun5373@naver.com>
2024-03-05 11:26:59 +0900

    ADD_Fog plane 추가

--- Assets/01.Scripts/StageObjects/InteractableObjects/Goal/GoalObject.cs
+++ Assets/01.Scripts/StageObjects/InteractableObjects/Goal/GoalObject.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class GoalObject : InteractableObject
 {
-    private bool _isToggle;
-
-    public override void Awake()
-    {
-        base.Awake();
-        _isToggle = false;
-    }
-
     public override void OnInteraction(ObjectUnit communicator, bool interactValue, params object[] param)
     {
-        if(!_isToggle)
-        {
-            communicator.Converter.ConvertDimension(AxisType.None);
-            StageManager.Instance.StageClear();  
-            _isToggle = true;
-        }
+        communicator.Converter.ConvertDimension(AxisType.None);
+        StageManager.Instance.StageClear();
+        Activate(false);
     }
 }
