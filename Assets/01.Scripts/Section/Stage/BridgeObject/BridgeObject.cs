20219ad40f3965fde4507438705512f64157b23c
ehdbs28 <doyun5373@naver.com>
2024-03-12 17:54:24 +0900

    FEAT_섹션 생성 및 삭제 연출 구현

--- Assets/01.Scripts/Section/Stage/BridgeObject/BridgeObject.cs
+++ Assets/01.Scripts/Section/Stage/BridgeObject/BridgeObject.cs
@@ -1,9 +1,34 @@
-using System;
-using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class BridgeObject : PoolableMono
 {
+    private Section _section;
+    private List<Material> _materials;
+        
+    private readonly int _visibleProgressHash = Shader.PropertyToID("_VisibleProgress");
+
+    private void Awake()
+    {
+        _materials = new List<Material>();
+        var renderers = transform.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            if(!renderer.enabled)
+            {
+                continue;
+            }
+            
+            foreach (var material in renderer.materials) 
+            {
+                _materials.Add(material);    
+            }
+        }
+        
+        DOTween.Init(true, true, LogBehaviour.Verbose). SetCapacity(2000, 100);
+    }
+
     public void SetWidth(float width)
     {
         var scale = transform.localScale;
@@ -11,39 +36,40 @@ public class BridgeObject : PoolableMono
         transform.localScale = scale;
     }
     
-    public void Generate(Vector3 position, Quaternion rotation)
+    public void Generate(Vector3 position, Quaternion rotation, Section section)
     {
         transform.position = position - Vector3.up * 5;
         transform.rotation = rotation;
-        StartCoroutine(MoveRoutine(position, null));
+        _section = section;
+        
+        Dissolve(true, 2.5f);
+        transform.DOMove(position, 3f);
     }
 
     public void Remove()
     {
-        StartCoroutine(MoveRoutine(transform.position - Vector3.up * 5, () =>
-        {
-            SceneControlManager.Instance.DeleteObject(this);
-        }));
+        Dissolve(false, 3f);
+        transform.DOMove(transform.position - Vector3.up * _section.SectionData.sectionYDepth, 3f)
+            .OnComplete(() =>
+            {
+                SceneControlManager.Instance.DeleteObject(this);
+            });
     }
 
-    private IEnumerator MoveRoutine(Vector3 dest, Action CallBack)
+    private void Dissolve(bool on, float time)
     {
-        while (true)
-        {
-            var pos = transform.position;
-            var lerp = Vector3.Lerp(pos, dest, 0.1f);
-            transform.position = lerp;
-            
-            if (Vector3.Distance(pos, dest) <= 0.01f)
-            {
-                break;
-            }
-
-            yield return null;
-        }
+        var value = on ? 0f : 1f;
         
-        transform.position = dest;
-        CallBack?.Invoke();
+        foreach (var material in _materials)
+        {
+            material.SetFloat(_visibleProgressHash, Mathf.Abs(1f - value));
+        }
+
+        foreach (var material in _materials)
+        {
+            DOTween.To(() => material.GetFloat(_visibleProgressHash),
+                progress => material.SetFloat(_visibleProgressHash, progress), value, time);
+        }
     }
 
     public override void OnPop()
