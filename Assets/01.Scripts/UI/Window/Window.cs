1f91e7b8611529f515e50a4c6eb62e507ab5debe
ehdbs28 <doyun5373@naver.com>
2024-03-19 19:31:23 +0900

    ADD_설정 윈도우 디자인 완료

--- Assets/01.Scripts/UI/Window/Window.cs
+++ Assets/01.Scripts/UI/Window/Window.cs
@@ -3,11 +3,4 @@ using UnityEngine;
 
 public class Window : ObjectUnit
 {
-    [SerializeField] private Canvas _canvas;
-    
-    public override void Init(AxisConverter converter)
-    {
-        base.Init(converter);
-        _canvas.worldCamera = CameraManager.Instance.MainCam;
-    }
 }
