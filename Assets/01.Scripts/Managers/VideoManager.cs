4f446d05ba4e1fd19ede5e56032b61c94a1a392a
ehdbs28 <doyun5373@naver.com>
2024-03-23 20:47:34 +0900

    FEAT_VideoManager 기능 제작

--- Assets/01.Scripts/Managers/VideoManager.cs
+++ Assets/01.Scripts/Managers/VideoManager.cs
@@ -1,14 +1,57 @@
+using System.Collections.Generic;
 using ManagingSystem;
+using UnityEngine;
 
 public enum QualityType
 {
-    
+    Low = 0,
+    Medium = 1,
+    High = 2
 }
 
 public class VideoManager : BaseManager<VideoManager>
 {
+    [Header("Resolution")] 
+    private Resolution[] _resolutions;
+    
+    [Header("Full Screen")] 
+    [SerializeField] private bool _defalutFullScreen;
+    
+    [Header("Quality")] 
+    [SerializeField] private QualityType _defalutQuality;
+
+    public override void Init()
+    {
+        base.Init();
+        _resolutions = Screen.resolutions;
+    }
+
     public override void StartManager()
     {
-        
+        SetResolution(_resolutions.Length - 1);
+        SetFullScreen(_defalutFullScreen);
+        SetQuality(_defalutQuality);
     }
+
+    public void SetResolution(int index)
+    {
+        var correctIndex = Mathf.Clamp(index, 0, _resolutions.Length - 1);
+
+        var resolution = _resolutions[correctIndex];
+        var width = resolution.width;
+        var height = resolution.height;
+
+        Screen.SetResolution(width, height, Screen.fullScreen);
+    }
+    
+    public void SetFullScreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+
+    public void SetQuality(QualityType type)
+    {
+        QualitySettings.SetQualityLevel((int)type);
+    }
+
 }
