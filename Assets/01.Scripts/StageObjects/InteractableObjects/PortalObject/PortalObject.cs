9d0e59f6e573914dda1c8af4ab7ff579045428d0
ehdbs28 <doyun5373@naver.com>
2024-03-26 10:40:24 +0900

    FEAT_포탈 기능 제작

--- Assets/01.Scripts/StageObjects/InteractableObjects/PortalObject/PortalObject.cs
+++ Assets/01.Scripts/StageObjects/InteractableObjects/PortalObject/PortalObject.cs
@@ -1,4 +1,3 @@
-using System;
 using InteractableSystem;
 using AxisConvertSystem;
 using UnityEngine;
@@ -9,38 +8,35 @@ public class PortalObject : InteractableObject
     [SerializeField] private AxisType _portalAxis;
 
     [SerializeField] private float _portalOutDistance = 1f;
-    
-    private ObjectUnit _parentUnit;
-
-    public override void Awake()
-    {
-        base.Awake();
-        _parentUnit = transform.parent.GetComponent<ObjectUnit>();
-    }
 
     public override void OnInteraction(ObjectUnit communicator, bool interactValue, params object[] param)
     {
         if (communicator is PlayerUnit playerUnit)
         {
-            var destPos = _linkedPortal._parentUnit.transform.localPosition;
-            destPos.SetAxisElement(_linkedPortal._portalAxis, destPos.GetAxisElement(_linkedPortal._portalAxis) * _portalOutDistance);
+            var center = _linkedPortal.Collider.bounds.center;
+            var dest = center + Vector3ExtensionMethod.GetAxisDir(_linkedPortal._portalAxis) * _portalOutDistance;
             
-            playerUnit.SetPosition(destPos);
+            playerUnit.SetPosition(dest);
         }
     }
     
-// #if UNITY_EDITOR
-//
-//     private void OnValidate()
-//     {
-//         var scale = Vector3.one;
-//         scale.SetAxisElement(_portalAxis, 0);
-//         var pos = Vector3ExtensionMethod.GetAxisDir(_portalAxis);
-//         pos.SetAxisElement(_portalAxis, pos.GetAxisElement(_portalAxis) * 0.51f);
-//         
-//         transform.localScale = scale;
-//         transform.localPosition = pos;
-//     }
-//
-// #endif
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        if (_portalAxis == AxisType.X)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        else if (_portalAxis == AxisType.Y)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (_portalAxis == AxisType.Z)
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 0);
+        }
+    }
+
+#endif
 }
