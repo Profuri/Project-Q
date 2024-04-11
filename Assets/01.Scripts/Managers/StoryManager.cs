aa139d01fc67463dd2271739919ab58d6c6b74d7
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2024-04-11 09:30:23 +0900

    ADD_Asset

---
+++ Assets/01.Scripts/Managers/StoryManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ManagingSystem;
+
+public class StoryManager : BaseManager<StoryManager>
+{
+    public override void StartManager()
+    {
+
+    }
+
+    public void ShowMessage(string message)
+    {
+
+    }
+}
