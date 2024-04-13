b349c251243aff9f57142e8016d0b794fa98560b
ehdbs28 <doyun5373@naver.com>
2024-04-13 19:48:10 +0900

    CHORE_챕터씬 타임라인 구성 완료

--- Assets/15.Timeline/CustomTrack/DissolveObject/DissolveObjectMixerBehaviour.cs
+++ Assets/15.Timeline/CustomTrack/DissolveObject/DissolveObjectMixerBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -15,14 +16,18 @@ public class DissolveObjectMixerBehaviour : PlayableBehaviour
     // NOTE: This function is called at runtime and edit time.  Keep that in mind when setting the values of properties.
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
+        if (!Application.isPlaying)
+        {
+            return;
+        }
+        
         var inputCount = playable.GetInputCount();
 
         if (inputCount == 0)
         {
-            _isFirstFrame = true;
             return;
         }
-
+        
         if (_isFirstFrame)
         {
             MaterialListSetting(playerData as GameObject);
@@ -30,7 +35,15 @@ public class DissolveObjectMixerBehaviour : PlayableBehaviour
         }
 
         var weight = playable.GetInputWeight(0);
-        SetMaterialValue(weight);
+
+        if (weight == 0f)
+        {
+            _isFirstFrame = true;
+        }
+        else
+        {
+            SetMaterialValue(weight);
+        }
     }
 
     private void MaterialListSetting(GameObject bidingObject)
