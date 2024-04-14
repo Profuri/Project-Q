c0b75850c9d176d70f9446957460ce6759cad067
ehdbs28 <doyun5373@naver.com>
2024-04-14 22:42:44 +0900

    ADD_챕터 클리어 타임라인 추가

---
+++ Assets/15.Timeline/CustomTrack/CinemachineHolder/CinemachineHolderMixerBehaviour.cs
@@ -0,0 +1,27 @@
+using Cinemachine;
+using UnityEngine.Playables;
+
+public class CinemachineHolderMixerBehaviour : PlayableBehaviour
+{
+    // NOTE: This function is called at runtime and edit time.  Keep that in mind when setting the values of properties.
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        var inputCount = playable.GetInputCount();
+        var totalWeight = 0f;
+
+        for (int i = 0; i < inputCount; i++)
+        {
+            var inputWeight = playable.GetInputWeight(i);
+            totalWeight += inputWeight;
+        }
+
+        var bindingVirtualCam = playerData as CinemachineVirtualCamera;
+
+        if (bindingVirtualCam == null)
+        {
+            return;
+        }
+
+        bindingVirtualCam.m_Priority = totalWeight <= 0f ? 0 : 500;
+    }
+}
