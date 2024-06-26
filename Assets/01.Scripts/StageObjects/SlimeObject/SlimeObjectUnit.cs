9047e1f924b550d728feabd95a37d1d5a7995e31
ehdbs28 <doyun5373@naver.com>
2024-06-26 11:05:01 +0900

    FEAT_슬라임 사이즈 이슈 수정

--- Assets/01.Scripts/StageObjects/SlimeObject/SlimeObjectUnit.cs
+++ Assets/01.Scripts/StageObjects/SlimeObject/SlimeObjectUnit.cs
@@ -133,12 +133,12 @@ public class SlimeObjectUnit : ObjectUnit
     {
         float appendTime = 0.15f;
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DOScale(1.2f, appendTime));
-        sequence.Append(transform.DOScale(0.8f, appendTime));
-        sequence.Append(transform.DOScale(1f, appendTime));
-        sequence.Append(transform.DOScale(1.2f, appendTime));
-        sequence.Append(transform.DOScale(0.8f, appendTime));
-        sequence.Append(transform.DOScale(1f, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale * 1.2f, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale * 0.8f, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale * 1.2f, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale * 0.8f, appendTime));
+        sequence.Append(transform.DOScale(OriginUnitInfo.LocalScale, appendTime));
         sequence.AppendCallback(() => Callback?.Invoke());
     }
 
